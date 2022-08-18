c2bcf614343a93b4c782a947dbe5e0dc12a6e681
Steven-Otieno <steveotianga8991@gmail.com>
2022-08-18 11:04:16 +0300

    Assignment Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    array_length = array.size
+    case 
+    when array_length == 1
+        return array.join
+    when array_length == 2
+        and_value = array.pop
+        array.push(" and ")
+        array.push(and_value)
+        return array.join
+    when array_length >= 3
+        and_value = array.pop
+        array.push("and ")
+        add = array.join(', ')
+        add = add + and_value
+        return add
+        
+    end
 end
+
+oxford_comma(["fiddleheads", "okra", "kohlrabi"])
